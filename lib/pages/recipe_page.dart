5a1b3985982f671ca1cf9be02f673f153431cae0
Toke Friis <tokefriis@gmail.com>
2024-04-15 13:08:41 +0200

    made better recipe_page

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -14,39 +14,43 @@ class RecipePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Opskrift"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              recipe.name,
-              style: textTheme.titleLarge,
-            ),
-            Text(
-              recipe.description,
-              style: textTheme.labelMedium,
-            ),
-            Text(
-                "${recipe.instructions.length} steps, ${recipe.durationInMins} min."),
-            ListView.builder(
-              itemCount: recipe.ingredientsForRecipe.length,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: Text("${recipe.ingredientsForRecipe[index].quantity} ${recipe.ingredientsForRecipe[index].unit} ${recipe.ingredientsForRecipe[index].name}"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                recipe.name,
+                style: textTheme.titleLarge,
               ),
-              shrinkWrap: true,
-            ),
-            ListView.builder(
-              itemCount: recipe.instructions.length,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: Text("${index + 1}. ${recipe.instructions[index]}"),
+              Text(
+                recipe.description,
+                style: textTheme.labelMedium,
               ),
-              shrinkWrap: true,
-            ),
-            Text("Total pris: ${recipe.getTotalPrice.roundToDouble()}")
-          ],
+              Text(
+                  "${recipe.instructions.length} steps, ${recipe.durationInMins} min."),
+              ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: recipe.ingredientsForRecipe.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Text("${recipe.ingredientsForRecipe[index].quantity} ${recipe.ingredientsForRecipe[index].unit} ${recipe.ingredientsForRecipe[index].name}"),
+                ),
+                shrinkWrap: true,
+              ),
+              ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: recipe.instructions.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Text("${index + 1}. ${recipe.instructions[index]}"),
+                ),
+                shrinkWrap: true,
+              ),
+              Text("Total pris: ${recipe.getTotalPrice.roundToDouble()}")
+            ],
+          ),
         ),
       ),
     );
